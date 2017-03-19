bbda406de09994e7ba9ec22d823d550374b2be46
Michael Kohler <me@michaelkohler.info>
2017-03-19 19:16:27 +0100

    Better list of Reps

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,13 @@ const storageHandler = new StorageHandler();
 /* GET home page. */
 router.get('/', (req, res, next) => {
   const reps = storageHandler.getStorageItem('reps');
+  reps.forEach((rep) => {
+    rep.isMentor = false;
+
+    rep.profile.groups.forEach((group) => {
+      if (group.name === 'Mentor') rep.isMentor = true;
+    });
+  });
 
   res.render('index', {
     title: 'Reps Role Focus Tracking',
